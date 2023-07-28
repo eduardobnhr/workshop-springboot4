558f0fc03e75c66f8ad5c93c51f28c9680f5197b
eduardo <eduardobnhr@gmail.com>
2023-07-28 11:10:48 -0400

    user entity and resource

---
+++ src/main/java/com/servicosweb/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.servicosweb.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.servicosweb.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "eduardo", "eduardo@gmail.com", "992676589", "350206");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
